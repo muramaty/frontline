2a9c351e0ef8742c5cc5d1dfe7e05b75b9bc781e
muramaty <osaru9a-red@yahoo.co.jp>
2021-09-05 17:44:46 +0900

    トップページ作成、webpack, yarn, tachyons-css の準備

---
+++ src/home.js
@@ -0,0 +1,2 @@
+import './css/home.css'
+//import "tachyons";
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  // モードの設定
+  mode: 'development',
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:['style-loader',
+             'css-loader'
+                /*
+             {  loader: 'css-loader',
+                options: {//modules: true,
+                  modules: {
+                    localIdentName: '[name]',
+                  },
+                  importLoaders: 1
+                }
+             },
+             'postcss-loader' */
+        ]
+      }
+    ]
+  },
+
+  // エントリーポイントの設定
+  entry: {
+    home: `./src/home.js`,
+    //jq: `./src/jq.js`,
+  },
+
+  // ファイルの出力設定
+  output: {
+    // 出力するファイル名
+    filename: "[name].js",
+
+    //  出力先のパス
+    // path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'public/js')
+  },
+
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   scriptLoading: 'defer',
+    // }),
+    /*
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+*/
+  ],
+
+};
